e5117592f2539c837af932908bcbee0543b3c936
Jorge Leon <jrleon@pucp.edu.pe>
2020-11-07 18:40:59 -0500

    Spring Initialization

---
+++ src/main/java/com/Bono/Peru/Backend/BonoPeruBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Bono.Peru.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonoPeruBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonoPeruBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bono/Peru/Backend/BonoPeruBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bono.Peru.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonoPeruBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
