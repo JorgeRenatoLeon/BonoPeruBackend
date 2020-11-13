aba16fc6e26d7ea537cf83c9074950658ac0fcd0
solojoh151296 <54746679+solojoh151296@users.noreply.github.com>
2020-11-12 22:29:12 -0500

    comentarios

--- src/main/java/com/bonoperubackend/BonoPeruBackend/AlgoritmoGenético/Individual.java
+++ src/main/java/com/bonoperubackend/BonoPeruBackend/AlgoritmoGenético/Individual.java
@@ -5,10 +5,14 @@ import java.util.Collection;
 import java.util.Hashtable;
 
 public class Individual {
-    private ArrayList<Integer> fitness=new ArrayList<>();
+   // Implementa el individuo del AG. Un individuo tiene un cromosoma que es una lista de NUM_ITEMS elementos (genes),
+   //  cada gen i puede asumir dos posibles alelos: 0 o 1 (no incluir/incluir en la mochila el item i del pool) """
+    private ArrayList<Integer> fitness=new ArrayList<>(); //inicializa
     private ArrayList<Integer> chromosome;
+    //Constructor con el mismo nombre de la clase
 
     public ArrayList<Integer> getFitness() {
+
         return fitness;
     }
 
@@ -25,10 +29,35 @@ public class Individual {
     }
 
     public ArrayList<Individual> crossover_onepoint(Individual individual) {
+        /*"Retorna dos nuevos individuos del cruzamiento de un punto entre individuos self y other "
+        c = randrange(len(self.chromosome))
+        ind1 = Individual(self.chromosome[:c] + other.chromosome[c:])
+        ind2 = Individual(other.chromosome[:c] + self.chromosome[c:])
+        return [ind1, ind2]*/
+
         return new ArrayList<Individual>();
     }
 
     public ArrayList<Individual>  crossover_uniform(Individual individual) {
+        /*
+        chromosome1 = []
+        chromosome2 = []
+        "Retorna dos nuevos individuos del cruzamiento uniforme entre self y other "
+        for i in range(len(self.chromosome)):
+            if uniform(0, 1) < 0.5:
+                chromosome1.append(self.chromosome[i])
+                chromosome2.append(other.chromosome[i])
+            else:
+                chromosome1.append(other.chromosome[i])
+                chromosome2.append(self.chromosome[i])
+        ind1 = Individual(chromosome1)
+        ind2 = Individual(chromosome2)
+        return [ind1, ind2]
+         */
+
+
+
+
         return new ArrayList<Individual>();
     }
 
