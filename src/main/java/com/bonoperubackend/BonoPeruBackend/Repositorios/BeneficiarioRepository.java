fdf5603be8476a746264af401a916e3e0f19e889
solojoh151296 <54746679+solojoh151296@users.noreply.github.com>
2020-11-15 21:22:34 -0500

    probando lo de repository

--- src/main/java/com/bonoperubackend/BonoPeruBackend/Repositorios/BeneficiarioRepository.java
+++ src/main/java/com/bonoperubackend/BonoPeruBackend/Repositorios/BeneficiarioRepository.java
@@ -1,9 +1,17 @@
 package com.bonoperubackend.BonoPeruBackend.Repositorios;
 
 import com.bonoperubackend.BonoPeruBackend.Modelos.Beneficiario;
+import com.bonoperubackend.BonoPeruBackend.Modelos.Horario;
+import com.bonoperubackend.BonoPeruBackend.Modelos.RespuestaEncuesta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
+
 public interface BeneficiarioRepository extends  JpaRepository<Beneficiario,Integer>{
+    //Optional<Horario> findAllByBeneficiario(Integer id);
+
+  //Optional<Beneficiario> findByCodigo_familia(String codigo_familia);
+
 }
